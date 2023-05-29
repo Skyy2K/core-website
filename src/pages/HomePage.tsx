532ed3b7723df94d6e704a8607fa5d854b796128
Skyy <110974688+Skyy2K@users.noreply.github.com>
2023-05-29 20:04:47 +0100

    remove serving... page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { HomePageStyle, MainButton } from '../utils/styles';
 
-import { FaPlusCircle, FaDiscord, FaUserAlt, FaServer } from "react-icons/fa";
+import { FaPlusCircle, FaDiscord } from "react-icons/fa";
 
 import logo from "../icons/logo.png";
 
@@ -15,30 +15,6 @@ export const HomePage = () => {
     window.location.href = "/support";
   };
 
-  const [botData, setBotData] = React.useState({ users: 0, guilds: 0 });
-
-  const fetchBotData = () => {
-    fetch("http://2.223.144.35:3284/api/bot-stats")
-    .then((response) => {
-      console.log(response)
-      return response.json();
-    })
-    .then((data) => {
-      setBotData(data);
-    });
-  };
-  
-  React.useEffect(() => {
-    fetchBotData();
-  });
-
-  function roundThing(number: number) {
-    const numberStr = `${number}`;
-    const lengthWithoutFirst = numberStr.length - 1;
-    console.log(numberStr)
-    return parseInt(numberStr.charAt(0) + "0".repeat(lengthWithoutFirst).toLocaleString()).toLocaleString();
-  };
-
   return (
     <HomePageStyle>
         <div>
@@ -48,7 +24,6 @@ export const HomePage = () => {
           </div>
           <p>
             Create Custom Commands for your server with ease.
-            Serving {botData.guilds} servers.
           </p>
         </div>
         <div style={{ flexDirection: 'row' }}>
@@ -62,20 +37,6 @@ export const HomePage = () => {
                 <p style={{ fontSize: '18px' }}>Support</p>
             </MainButton>
             <hr></hr>
-        </div>
-        
-        <div>
-          <h1>Serving...</h1>
-          <hr></hr>
-          <MainButton style={{ cursor: 'default' }}>
-            <FaUserAlt size={45} />
-            <p style={{ fontSize: '18px' }}><strong>+{roundThing(botData.users)}</strong> users</p>
-          </MainButton>
-          <MainButton style={{ cursor: 'default' }}>
-            <FaServer size={45} />
-            <p style={{ fontSize: '18px' }}><strong>+{roundThing(botData.guilds)}</strong> servers</p>
-          </MainButton>
-          <hr></hr>
         </div>
         <div style={{ display: 'flex', width: '250px', justifyContent: 'space-between' }}>
             <a href="/privacy">Privacy Policy</a>
